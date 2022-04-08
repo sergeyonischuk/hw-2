c76ed0a4deb32b10008af38857694d9f7601a6da
Sergey Onischuk <sergey.onischuk@ukr.net>
2022-04-08 16:51:18 +0300

    homework 2 is done

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,21 @@
+public class NameEncoderDecoder {
+    public static void main(String[] args) {
+    }
+
+    public static String encode(String name) {
+        return "NOTFORYOU"+name.replace('e', '1')
+                .replace('u', '2')
+                .replace('i', '3')
+                .replace('o', '4')
+                .replace('a','5')
+                +"YESNOTFORYOU";
+    }
+    public static String decode(String name) {
+        return name.replaceAll("YESNOTFORYOU", "")
+                .replace('1', 'e')
+                .replace('2', 'u')
+                .replace('3', 'i')
+                .replace('4', 'o')
+                .replace('5','a').replaceAll("NOTFORYOU", "");
+    }
+}
